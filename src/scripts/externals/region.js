d20bdb971daa45187c1fb79414a6be3fe135b9aa
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-20 16:15:40 -0300

    Update region handlers/styles

---
+++ src/scripts/externals/region.js
@@ -0,0 +1,3 @@
+/* Replace first letter of region name to upper case */
+var regionInfo = document.querySelector("h1");
+regionInfo.innerText = regionInfo.innerText.replace(regionInfo.innerText[0], regionInfo.innerText[0].toUpperCase());
